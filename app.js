14afd735678b0225bdc7e9051701e5b81aa26c85
Deetsy <amardeepdeepak2016@gmail.com>
2024-08-09 22:46:17 +0530

    add environment variables

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const usersRouter = require("./routes/usersRoutes");
+const usersRouter = require("./routes/usersRouter");
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.get("/", usersRouter);
+app.use("/", usersRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
